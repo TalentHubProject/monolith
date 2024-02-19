60716f7a2c4c971d0a77d21976d8452b3923035d
Leonarddoo <samuel.pomin@gmail.com>
2024-02-19 18:34:17 +0100

    feat(API): Liaison avec l'API pour récupérer les créatures

--- talent-hub-aleks_bot/app/src/main/java/org/talenthub/module/xp/repository/PlayerLevelRepository.java
+++ talent-hub-aleks_bot/app/src/main/java/org/talenthub/module/xp/repository/PlayerLevelRepository.java
@@ -2,7 +2,7 @@ package org.talenthub.module.xp.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import org.talenthub.module.xp.persistence.PlayerLevel;
+import org.talenthub.module.xp.entity.PlayerLevel;
 
 @Repository
 public interface PlayerLevelRepository extends JpaRepository<PlayerLevel, Long> {
