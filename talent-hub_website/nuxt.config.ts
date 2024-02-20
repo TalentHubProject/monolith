6d6086c69f2c71b9f42978377130fad7639c5580
Alexis Chân Gridel <alexis.gridel@pm.me>
2024-02-20 20:50:11 +0100

    fix(vercel): deployement

--- talent-hub_website/nuxt.config.ts
+++ talent-hub_website/nuxt.config.ts
@@ -1,7 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  ssr: true,
+  routeRules: {
+    '/': { prerender: true },
+  },
   css: [
     "/assets/css/main.css"
   ],
