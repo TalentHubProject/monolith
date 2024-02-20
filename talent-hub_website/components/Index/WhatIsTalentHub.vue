c2c35eb3651d7e58738c0069e4358f275e7dc5d2
Alexis Chân Gridel <alexis.gridel@pm.me>
2024-02-20 13:49:33 +0100

    feat(component/what): more spacing in md

--- talent-hub_website/components/Index/WhatIsTalentHub.vue
+++ talent-hub_website/components/Index/WhatIsTalentHub.vue
@@ -3,61 +3,51 @@
 </script>
 
 <template>
-  <section class="mt-8 pb-8 md:mt-32">
-    <div class="border-solid md:border-2 border-blue-500 m-3 rounded-xl">
-      <div
-          class="mt-2 md:16 pt-32 px-4 mx-auto max-w-screen-xl text-center lg:pt-16"
-      >
-        <div class="flex items-center justify-center">
-          <div class="bg-slate-200 px-4 py-1 rounded text-slate-700">
-            <img
-                class="float-left pt-1 pr-2"
-                src="~/assets/images/file.png"
-            />
-            A propos
-          </div>
+    <section class="mt-8 pb-8 md:mt-32">
+        <div class="border-2 border-blue-500 m-3 rounded-xl">
+            <div class="pt-16 px-4 mx-auto max-w-screen-xl text-center">
+                <div class="flex justify-center">
+                    <div class="flex items-center bg-slate-200 px-4 py-1 rounded text-slate-700">
+                        <img alt="Icone A propos" class="mr-2" src="~/assets/images/file.png"/>
+                        A propos
+                    </div>
+                </div>
+
+                <h1 class="mt-12 text-2xl md:text-3xl font-bold">
+                    Qu'est-ce que Talent Hub ?
+                </h1>
+                <p class="mt-8 text-lg lg:text-xl font-normal text-gray-500 sm:px-16 xl:px-64 dark:text-gray-400">
+                    Découvrez Talent Hub, la plateforme collaborative ultime qui rassemble les esprits créatifs et
+                    innovants.
+                </p>
+
+                <div class="mt-20 md:pb-16 grid grid-cols-1 md:grid-cols-3 gap-x-8 gap-y-16 md:gap-y-5 md:gap-x-24 text-justify px-0 md:px-12 text-left">
+                    <div>
+                        <img alt="Contenu" src="~/assets/images/Ellipse1.png"/>
+                        <h3 class="pt-4 text-2xl font-semibold">Contenu</h3>
+                        <p class="my-2 text-md leading-8 text-gray-400">
+                            Suivez des créations ou un modèle créé par des artistes vérifiés et accélérez la croissance
+                            de votre niveau et de votre créativité.
+                        </p>
+                    </div>
+                    <div>
+                        <img alt="Communauté" src="~/assets/images/Ellipse2.png"/>
+                        <h3 class="pt-4 text-2xl font-semibold">Communauté</h3>
+                        <p class="my-2 text-md leading-8 text-gray-400">
+                            Rejoignez Talent Hub et trouvez les connaissances, le soutien et la communauté qui vous
+                            aideront à faire évoluer votre entreprise vers le niveau suivant.
+                        </p>
+                    </div>
+                    <div>
+                        <img alt="Besoin d’aide" src="~/assets/images/Ellipse3.png"/>
+                        <h3 class="pt-4 text-2xl font-semibold">Besoin d’aide</h3>
+                        <p class="my-2 text-md leading-8 text-gray-400">
+                            Recevez des conseils de plusieurs artistes en groupe ou en tête-à-tête et surmontez les
+                            obstacles commerciaux et les blocages créatifs.
+                        </p>
+                    </div>
+                </div>
+            </div>
         </div>
-        <h1 class="text-2xl md:text-3xl pt-12 font-bold">
-          Qu'est-ce que Talent Hub ?
-        </h1>
-        <p
-            class="text-lg pt-8 font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-64 dark:text-gray-400"
-        >
-          Découvrez Talent Hub, la plateforme collaborative ultime qui
-          rassemble les esprits créatifs et innovants.
-        </p>
-        <div
-            class="px-0 md:px-12 mt-20 md:pb-16 grid grid-cols-1 gap-16 md:grid-cols-3 md:gap-5 text-left"
-        >
-          <div>
-            <img class="" src="~/assets/images/Ellipse1.png"/>
-            <h3 class="pt-4 text-2xl font-semibold">Contenu</h3>
-            <p class="pt-4 pb-4 font-normal text-xl text-gray-400 text-justify">
-              Suivez des créations ou un modèle créé par des artistes vérifiés
-              et accélérez la croissance de votre niveau et de votre
-              créativité.
-            </p>
-          </div>
-          <div>
-            <img class="" src="~/assets/images/Ellipse2.png"/>
-            <h3 class="pt-4 text-2xl font-semibold">Communauté</h3>
-            <p class="pt-4 pb-4 font-normal text-xl text-gray-400 text-justify">
-              Rejoignez Talent Hub et trouvez les connaissances, le soutien et
-              la communauté qui vous aideront à faire évoluer votre entreprise
-              vers le niveau suivant.
-            </p>
-          </div>
-          <div>
-            <img class="" src="~/assets/images/Ellipse3.png"/>
-            <h3 class="pt-4 text-2xl font-semibold">Besoin d’aide</h3>
-            <p class="pt-4 pb-4 font-normal text-xl text-gray-400 text-justify">
-              Recevez des conseils de plusieurs artistes en groupe ou en
-              tête-à-tête et surmontez les obstacles commerciaux et les
-              blocages créatifs.
-            </p>
-          </div>
-        </div>
-      </div>
-    </div>
-  </section>
+    </section>
 </template>
