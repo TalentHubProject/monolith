e14aaa5703dc095d944306ab1b7c6c5d62505979
Alexis Chân Gridel <alexis.gridel@pm.me>
2024-02-23 20:12:40 +0100

    fix(nuxt): remove route rule

--- talent-hub_website/nuxt.config.ts
+++ talent-hub_website/nuxt.config.ts
@@ -1,9 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  routeRules: {
-    '/': { prerender: true },
-  },
   css: [
     "/assets/css/main.css"
   ],
