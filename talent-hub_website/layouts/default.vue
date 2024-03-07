595a0b595b9deff50c3ef3e96ee41a1a8c09be69
Alexis Chân Gridel <alexis.gridel@pm.me>
2024-03-07 16:53:47 +0100

    feat: layout meta head

--- talent-hub_website/layouts/default.vue
+++ talent-hub_website/layouts/default.vue
@@ -1,37 +1,47 @@
-
 <template>
-  <div >
+    <div>
 
-   <Navbar/>
-       <slot />
-      <SpeedInsights />
-    <Footer/>
-  </div>
+        <Navbar/>
+        <slot/>
+        <SpeedInsights/>
+        <Footer/>
+    </div>
 </template>
 <script>
 
 import './assets/css/main.css'
-import { SpeedInsights } from "@vercel/speed-insights/nuxt"
 
 export default {
-  data() {
-    return {
-      isMenuHidden: true
-    };
-  },
-  methods: {
-    toggleMenu() {
-      this.isMenuHidden = !this.isMenuHidden;
+    data() {
+        return {
+            isMenuHidden: true
+        };
+    },
+    methods: {
+        toggleMenu() {
+            this.isMenuHidden = !this.isMenuHidden;
+        }
+    },
+    setup() {
+        useSeoMeta({
+            title: 'Talent Hub - Serveur Discord d\'entraide pour artistes, développeurs, musiciens et entrepreneurs',
+            description: 'Rejoignez Talent Hub, la communauté Discord pour l\'entraide et la collaboration dans les domaines de l\'art, du développement, de la musique, de l\'investissement et de l\'audiovisuel. Trouvez des offres d\'emploi, des missions freelance et lancez vos projets avec d\'autres passionnés.',
+            image: 'https://talent-hub.fr/assets/images/og.png',
+            url: 'https://talent-hub.fr',
+            author: 'Talent Hub',
+            robots: 'index, follow',
+            viewport: 'width=device-width, initial-scale=1',
+            ogTitle: 'Talent Hub - Serveur Discord d\'entraide pour artistes, développeurs, musiciens et entrepreneurs',
+            ogDescription: 'Rejoignez Talent Hub, la communauté Discord pour l\'entraide et la collaboration dans les domaines de l\'art, du développement, de la musique, de l\'investissement et de l\'audiovisuel. Trouvez des offres d\'emploi, des missions freelance et lancez vos projets avec d\'autres passionnés.',
+            ogImage: 'https://talent-hub.fr/assets/images/og.png',
+            ogUrl: 'https://talent-hub.fr',
+            ogType: 'website',
+            twitterTitle: 'Talent Hub - Serveur Discord d\'entraide pour artistes, développeurs, musiciens et entrepreneurs',
+            twitterDescription: 'Rejoignez Talent Hub, la communauté Discord pour l\'entraide et la collaboration dans les domaines de l\'art, du développement, de la musique, de l\'investissement et de l\'audiovisuel. Trouvez des offres d\'emploi, des missions freelance et lancez vos projets avec d\'autres passionnés.',
+            twitterImage: 'https://talent-hub.fr/assets/images/og.png',
+            twitterCard: 'summary_large_image',
+            keywords: "discord, communauté, entraide, artistes, développeurs, musiciens, entrepreneurs, investissement, audiovisuel, emploi, freelance, projets, passionnés, serveur discord, bot discord, discord web, pixel art, pixel art facile, art, street art, jobs"
+        })
     }
-  },
-  setup() {
-    useSeoMeta({
-      title: 'Talent Hub',
-      description: 'Réunissant passionnés d\'art, de développement, de musique et bien plus encore',
-      image: 'https://talenthub.fr/assets/images/og.png',
-      url: 'https://talenthub.fr'
-    })
-  }
 };
 </script>
-<style lang="scss" scoped></style>
