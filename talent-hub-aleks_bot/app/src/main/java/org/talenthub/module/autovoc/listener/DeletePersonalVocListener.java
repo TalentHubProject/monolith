8ff2f6975bcdbe29f66ff3f30929a674008b1b19
Leonarddoo <samuel.pomin@gmail.com>
2024-03-07 15:42:39 +0100

    fix(DeletePersonnalVoc): Fix pour pas supprimer le salon d'autovoc

--- talent-hub-aleks_bot/app/src/main/java/org/talenthub/module/autovoc/listener/DeletePersonalVocListener.java
+++ talent-hub-aleks_bot/app/src/main/java/org/talenthub/module/autovoc/listener/DeletePersonalVocListener.java
@@ -1,18 +1,24 @@
 package org.talenthub.module.autovoc.listener;
 
+import lombok.AllArgsConstructor;
 import net.dv8tion.jda.api.events.guild.voice.GuildVoiceUpdateEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.springframework.stereotype.Component;
 import org.talenthub.module.autovoc.model.PersonalVocal;
+import org.talenthub.service.ConfigService;
 
 @Component
+@AllArgsConstructor
 public class DeletePersonalVocListener extends ListenerAdapter {
+
+    private final ConfigService configService;
 
     @Override
     public void onGuildVoiceUpdate(GuildVoiceUpdateEvent event) {
 
         if(event.getChannelLeft() == null) return;
         if(!event.getChannelLeft().getMembers().isEmpty()) return;
+        if(event.getChannelLeft().getId().equals(configService.getString("auto-voc-channel-id"))) return;
         if(!PersonalVocal.vocalMap.containsKey(event.getMember().getIdLong())) return;
 
         PersonalVocal.vocalMap.remove(event.getMember().getIdLong());
